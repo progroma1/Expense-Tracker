bbf1bfefdb5efc02716b9b9c1cafa0dab16186e0
Roman Tyniv <roman.tyniv@3ss.tv>
2023-03-20 23:02:31 +0200

    first settings

---
+++ screens/ManageExpense.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const ManageExpense = () => {
+  return (
+    <View>
+      <Text>ManageExpense</Text>
+    </View>
+  );
+};
+
+export default ManageExpense;
